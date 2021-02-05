0c9e358e6ef16200e01ea277c2c4dac59a8a4f03
SeonJungKim <ksj083895@gmail.com>
2021-02-06 01:52:43 +0500

    Slider에 대해 알아봅시다

--- SwiftUI_Slider_TUT/ContentView.swift
+++ SwiftUI_Slider_TUT/ContentView.swift
@@ -8,11 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+     //1.
+    @State private var sliderValue:Double = 25
+    var minimumValue = 0.0
+    var maximumValue = 100.0
+    
     var body: some View {
-        Text("Hello, world!")
+        VStack {
+            HStack {
+                     //Int 값 안에 더블값
+                Text("\(Int(minimumValue))")
+                
+                //2.
+                Slider(value: $sliderValue, in: minimumValue...maximumValue, step: 10)
+                    .accentColor(.orange)
+                
+                Text("\(Int(maximumValue))")
+            }
             .padding()
+            //3.
+            Text("슬라이더 값은 '\(sliderValue, specifier: "%.1f")' 입니다.")
+        }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
